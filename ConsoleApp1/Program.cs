980a8c4e6a010966a75abd9ce3685d669e30f010
vsproko1@gmail.com <vsproko1@gmail.com>
2021-10-25 16:05:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;//число до которого будем находить простые числа
+            int ty;
+            Console.WriteLine("Введите начальное число ");
+            ty = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите последние число ");
+            N = int.Parse(Console.ReadLine());//вводим N
+            for (int i = ty; i <= N; i++)
+            {
+                if (isSimple(i))
+                {
+                    Console.Write(i.ToString() + ",");
+                }
+            }
+        }
+        //метод который определяет простое число или нет
+        private static bool isSimple(int N)
+        {
+            bool tf = false;
+            //чтоб убедится простое число или нет достаточно проверить не делитсья ли 
+            //число на числа до его половины
+            for (int i = 2; i < (int)(N / 2); i++)
+            {
+                if (N % i == 0)
+                {
+                    tf = false;
+                    break;
+                }
+                else
+                {
+                    tf = true;
+                }
+            }
+            return tf;
+        }
+    }
+}
